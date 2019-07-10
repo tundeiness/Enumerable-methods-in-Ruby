e6b1cb049077e801f3e9376f725abe1637d62465
tundeiness <tundeiness@yahoo.co.uk>
2019-07-10 17:14:55 +0100

    implemented the "my each with index method in module" Enumerable

--- enumerable.rb
+++ enumerable.rb
@@ -1,4 +1,5 @@
 module Enumerable
+
   def my_each
     # your code here
     i = 0
@@ -7,4 +8,28 @@ module Enumerable
       i += 1
     end
   end
+
+  def my_each_with_index
+    i = 0
+    while i < self.length
+      yield(self[i], i)
+      i += 1
+    end
+
+  end
+
+  def my_select
+    my_select.my_each
+  end
+
+  def my_all?
+  end
+
+  def my_any?
+  end
 end
+
+
+arr = [4,9,2,9,56,4,0]
+
+arr.my_each {|ele| puts ele}
