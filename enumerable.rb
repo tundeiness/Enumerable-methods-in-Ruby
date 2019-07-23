9a438bdbed01bff999dc23f459c0e1950c7a0cea
tundeiness <tundeiness@yahoo.co.uk>
2019-07-23 15:27:31 +0100

    initialised RSpec testing for the project

--- enumerable.rb
+++ enumerable.rb
@@ -2,14 +2,15 @@
 
 module Enumerable
   # my_each method
+  extend self
   def my_each
-    if instance_of?(Array)
+    if self.instance_of?(Array)
       i = 0
-      until i == length
+      until i == self.length
         yield (self[i])
         i += 1
       end
-    elsif instance_of?(Hash)
+    elsif self.instance_of?(Hash)
       i = 0
       arr = to_a
       until i == arr.length
@@ -22,14 +23,14 @@ module Enumerable
 
   # my_each_with_index method
   def my_each_with_index
-    if instance_of?(Array)
+    if self.instance_of?(Array)
       i = 0
-      until i == length
+      until i == self.length
         yield(self[i], i)
         i += 1
       end
 
-    elsif instance_of?(Hash)
+    elsif self.instance_of?(Hash)
       i = 0
       arr = to_a
       until i == arr.length
@@ -40,41 +41,60 @@ module Enumerable
     end
   end
 
-  #my_select method
+  # my_select method
   def my_select
-    res=[]
-       self.my_each do |elem|
-          if yield(elem)
-           res << elem
-          end
-       end
-        res
+    res = []
+    my_each do |elem|
+      res << elem if yield(elem)
+    end
+    res
   end
 
-  #my_none? method
+  # my_none? method
   def my_none?
     truthy = false
-    self.my_select do |item|
-      unless yield item
-        truthy = true
-      end
+    my_select do |item|
+      truthy = true unless yield item
       break
     end
     truthy
   end
 
+  # my_count method
+  def my_count(_arg = nil)
+    cnt = 0
+    my_select { |ele| ele * 1 }
+  end
 
+  # my_any method
+  def my_any?(*_args)
+    my_each do |x|
+      if yield(x)
+        return false
+      else
+        return true
+      end
+    end
+  end
 end
 
 arr = [4, 9, 2, 9, 56, 4, 0]
 
 # {"h"=> 2, "v"=> 1, "l"=> 4, "m"=> 2, }.my_each{|k, v| puts v*2}
 
-# arr.my_each { |ele| puts ele }
+ arr.my_each { |ele| puts ele }
 
 # arr.my_each_with_index { |ele, i| puts ele }
 
-puts arr.my_select{ |ele| puts ele  > 4}
+# puts arr.my_select{ |ele| puts ele  > 4}
 
 # arr.my_all?{ |ele| puts ele  > 4}
+
 # arr.my_all? { |a| a >= 3 }
+
+# puts arr.my_none?{|a| a.nil? }
+
+# puts arr.my_count(4){|ele| puts ele == 2}
+
+#puts arr.my_any? { |ele| puts ele == 2 }
+#puts arr.any? { |ele| puts ele == 2 }
---
+++ spec/enumerable_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../enumerable.rb'
+
+
+
+describe Enumerable do
+
+  describe '#my_each' do
+    let(:arr){[1,2,3,4,5]}
+    it 'iterates and display each element of an array' do
+      expect()
+    end
+  end
+
+
+end
