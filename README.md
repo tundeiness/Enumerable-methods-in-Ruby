7f5cd94739929dc709f5e925414c07c679d0f397
tundeiness <tundeiness@yahoo.co.uk>
2019-07-26 09:37:33 +0100

    removed the RSpec test part of the project because of confusion, corrected some errors in the README file

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Enumerable-methods-in-Ruby
+
 This project is all about implementing some of Ruby's Enumerable Module methods.
-Methods implemented are the #each(#my_each), #each_with_index(#each_with_index), #select(#my_select), #all?(#my_all?), #any?(#my_any?),#none?(#my_none?),#count(#my_count), #map(#my_map), and #inject(#my_inject).
+Methods implemented are the #each(#my_each), #each_with_index(#each_with_index)
+,#select(#my_select), #all?(#my_all?), #any?(#my_any?),#none?(#my_none?)
+,#count(#my_count), #map(#my_map), and #inject(#my_inject).
 
 The #my_inject method was also tested in another method called #multiply_els.
 Although not required in the project, the Test Driven Development approach was
@@ -9,8 +12,9 @@ approach is for me to have a better understand of RSpec, the terminologies and
 how to write test efficiently.
 
 
-# Technologies used.
-Ruby
+# Technology used
+
+  Ruby
 
 
 
@@ -26,5 +30,6 @@ Ruby
 
 
 # Author
-This projects was executed by **Tunde**.
-You can meet me here on  [Github](https://github.com/tundeiness/) and,  here on [LinkedIn](https://www.linkedin.com/in/tunde-oretade/)
+
+  This project was executed by **Tunde**.
+  You can meet me here on [LinkedIn](https://www.linkedin.com/in/tunde-oretade/)
--- enumerable.rb
+++ enumerable.rb
@@ -10,7 +10,7 @@ module Enumerable
       end
   end
 
-  #my_each_with_index method
+  # my_each_with_index method
   def my_each_with_index
     i = 0
     until i == self.length
@@ -19,7 +19,7 @@ module Enumerable
     end
   end
 
-  #my_count method
+  # my_count method
   def my_count
     cnt = 0
     self.my_each{|ele|
@@ -33,38 +33,38 @@ module Enumerable
      cnt
   end
 
-  #my_select method
+  # my_select method
   def my_select
     res = [] if self.instance_of? Array
     res = {} if self.instance_of? Hash
 
     self.my_each {|item|
-        res.push(item) if yield(item) != false
+      res.push(item) if yield(item) != false
     }
     res
   end
 
 
-  #my_all? method
+  # my_all? method
   def my_all?
     all = true
     self.my_each{|x|
-        all = false if !yield(x)
+      all = false if !yield(x)
     }
      all
   end
 
 
-  #my_any? method
+  # my_any? method
   def my_any?
     any = false
     self.my_each{|x|
-        any = true if yield(x)
+      any = true if yield(x)
     }
      any
   end
 
-  #my_none? method
+  # my_none? method
   def my_none?
     none = false
     self.my_select{|x|
@@ -73,27 +73,27 @@ module Enumerable
     none
   end
 
-  #my_map method
+  # my_map method
   def my_map(proc = nil)
     result = []
     if proc
-      self.my_each{|elem|
+      self.my_each{ |elem|
         result.push(proc.call(elem))
       }
     else
-      self.my_each{|elem|
+      self.my_each{ |elem|
         result.push(yield(elem))
       }
     end
     result
   end
 
-  #my_inject method
+  # my_inject method
   def my_inject(*arg)
     init = arg.length > 0
     store = init ? arg[0] : self[0]
-    self.drop(init ? 0 : 1).my_each{|num|
-      store = yield(store,num)
+    self.drop(init ? 0 : 1).my_each{ |num|
+      store = yield(store, num)
     }
     store
   end
@@ -103,36 +103,26 @@ end
 
 def multiply_els(args)
 include Enumerable
-args.my_inject{|acc,num|
-    acc * num
+args.my_inject{ |acc, num|
+acc * num
 }
 end
 
 arr = [4, 9, 2, 9, 56, 4, 0]
 
-puts multiply_els([2,4,5])
-#arr = ['girl','boy','girl','girl']
-
-
+puts multiply_els([2, 4, 5])
+# arr = ['girl','boy','girl','girl']
 # arr.my_each{ |ele| puts ele }
-
- #arr.my_each_with_index { |ele, i| puts i }
-
- #puts arr.my_select{ |ele| puts ele  > 4}
-
+ # arr.my_each_with_index { |ele, i| puts i }
+ # puts arr.my_select{ |ele| puts ele  > 4}
 # arr.my_all?{ |ele| puts ele  > 4}
-
 # arr.my_all? { |a| a >= 3 }
-
- #puts arr.my_none?{|a| a.nil? }
- #puts arr.none?{|a| a.nil? }
- #puts arr.none?
- #puts %w{ant bear cat}.none? { |word| word.length == 5 }
- #puts %w{ant bear cat}.none? { |word| word.length >= 4 } #=> false
-
-#puts arr.my_count
-
-#puts arr.my_any? { |ele| puts ele == 2 }
-#puts arr.any? { |ele| puts ele == 2 }
-
-#puts arr.my_inject{ |sum,num| sum+num}
+ # puts arr.my_none?{|a| a.nil? }
+ # puts arr.none?{|a| a.nil? }
+ # puts arr.none?
+ # puts %w{ant bear cat}.none? { |word| word.length == 5 }
+ # puts %w{ant bear cat}.none? { |word| word.length >= 4 } #=> false
+# puts arr.my_count
+# puts arr.my_any? { |ele| puts ele == 2 }
+# puts arr.any? { |ele| puts ele == 2 }
+# puts arr.my_inject{ |sum,num| sum+num}
